897635602bd2221a8739b96cd3c7ba3beb2ae732
vozderjus <memkased@gmail.com>
2026-04-13 11:11:25 +0500

    feat: init pygame, venv, add constants

---
+++ src/core/constants.py
@@ -0,0 +1,23 @@
+import pygame
+
+TILE_SIZE = 32
+GRID_W = 16
+GRID_H = 16
+
+WIDTH = GRID_W * TILE_SIZE
+HEIGHT = GRID_H * TILE_SIZE
+FPS = 60
+
+COLORS = {
+    "bg": (18, 18, 24),
+    "grid_line": (40, 40, 50),
+    "hero": (76, 175, 80),
+    "exit": (255, 193, 7),
+    "text": (230, 230, 230),
+    "debug_overlay": (0, 0, 0, 180)
+}
+
+class GameState:
+    RUNNING = "RUNNING"
+    PAUSED = "PAUSED"
+    EXITING = "EXITING"
